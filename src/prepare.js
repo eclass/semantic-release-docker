ec8c3c6a4be3776bf6dfe1f71f5182ec31bcbecc
Gabriele <gabriele@gabrieles-mbp.lan>
2022-02-15 00:03:52 +0100

    feat: backwards compatibility for the latest tag

--- src/prepare.js
+++ src/prepare.js
@@ -1,6 +1,6 @@
 const AggregateError = require('aggregate-error')
 const Dockerode = require('dockerode')
-const { template } = require('lodash')
+const { template, uniq } = require('lodash')
 
 const getError = require('./get-error')
 
@@ -19,7 +19,7 @@ module.exports = async (pluginConfig, ctx) => {
   try {
     const docker = new Dockerode()
     const image = docker.getImage(pluginConfig.baseImageName)
-    const tags = [ctx.nextRelease.version]
+    let tags = [ctx.nextRelease.version]
     if (pluginConfig.additionalTags && pluginConfig.additionalTags.length > 0) {
       tags.push(...pluginConfig.additionalTags)
     }
@@ -28,16 +28,15 @@ module.exports = async (pluginConfig, ctx) => {
     if (!ctx.nextRelease.channel) {
       tags.push(baseImageTag)
     }
-    for (let tag of tags) {
-      tag = template(tag)(ctx)
+    tags = uniq(tags).map((tag) => template(tag)(ctx))
+    for (const tag of tags) {
       ctx.logger.log(
         `Tagging docker image ${pluginConfig.baseImageName} to ${pluginConfig.baseImageName}:${tag}`,
       )
       await image.tag({ repo: pluginConfig.baseImageName, tag })
     }
     for (const { imageName } of pluginConfig.registries) {
-      for (let tag of tags) {
-        tag = template(tag)(ctx)
+      for (const tag of tags) {
         ctx.logger.log(
           `Tagging docker image ${pluginConfig.baseImageName} to ${imageName}:${tag}`,
         )
